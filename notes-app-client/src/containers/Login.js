5c4d97add2eb1f14902465a0a034bec2175878cc
Ming Ho <hkboi108@hotmail.com>
2018-12-20 02:54:12 -0800

    Updated login page

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,132 +1,76 @@
 import React, { Component } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 import { Auth } from "aws-amplify";
-import {
-  HelpBlock,
-  FormGroup,
-  FormControl,
-  ControlLabel
-} from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
-import "./ChangeEmail.css";
+import { Link } from "react-router-dom";
 
-export default class ChangeEmail extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      code: "",
+      isLoading: false,
       email: "",
-      codeSent: false,
-      isConfirming: false,
-      isSendingCode: false
+      password: ""
     };
   }
 
-  validatEmailForm() {
-    return this.state.email.length > 0;
-  }
-
-  validateConfirmForm() {
-    return this.state.code.length > 0;
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
     });
-  };
-
-  handleUpdateClick = async event => {
-    event.preventDefault();
-
-    this.setState({ isSendingCode: true });
-
-    try {
-      const user = await Auth.currentAuthenticatedUser();
-      await Auth.updateUserAttributes(user, { email: this.state.email });
-
-      this.setState({ codeSent: true });
-    } catch (e) {
-      alert(e.message);
-      this.setState({ isSendingCode: false });
-    }
-  };
-
-  handleConfirmClick = async event => {
-    event.preventDefault();
-
-    this.setState({ isConfirming: true });
-
-    try {
-      await Auth.verifyCurrentUserAttributeSubmit("email", this.state.code);
-
-      this.props.history.push("/settings");
-    } catch (e) {
-      alert(e.message);
-      this.setState({ isConfirming: false });
-    }
-  };
-
-  renderUpdateForm() {
-    return (
-      <form onSubmit={this.handleUpdateClick}>
-        <FormGroup bsSize="large" controlId="email">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          text="Update Email"
-          loadingText="Updating…"
-          disabled={!this.validatEmailForm()}
-          isLoading={this.state.isSendingCode}
-        />
-      </form>
-    );
   }
 
-  renderConfirmationForm() {
-    return (
-      <form onSubmit={this.handleConfirmClick}>
-        <FormGroup bsSize="large" controlId="code">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            value={this.state.code}
-            onChange={this.handleChange}
-          />
-          <HelpBlock>
-            Please check your email ({this.state.email}) for the confirmation
-            code.
-          </HelpBlock>
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          text="Confirm"
-          loadingText="Confirm…"
-          isLoading={this.state.isConfirming}
-          disabled={!this.validateConfirmForm()}
-        />
-      </form>
-    );
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    try {
+     await Auth.signIn(this.state.email, this.state.password);
+     this.props.userHasAuthenticated(true);
+    } catch (e) {
+      console.log(e);
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
   }
 
   render() {
     return (
-      <div className="ChangeEmail">
-        {!this.state.codeSent
-          ? this.renderUpdateForm()
-          : this.renderConfirmationForm()}
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Link to="/login/reset">Forgot password?</Link>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in…"
+          />
+        </form>
       </div>
     );
   }
