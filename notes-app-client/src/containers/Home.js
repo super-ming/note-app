86bcb2a3836ce13a670f0e425e52f21cf185b47e
Ming Ho <hkboi108@hotmail.com>
2018-12-20 19:03:47 -0800

    Added sign up and login button to home page body

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem  } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   constructor(props) {
@@ -67,6 +68,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
